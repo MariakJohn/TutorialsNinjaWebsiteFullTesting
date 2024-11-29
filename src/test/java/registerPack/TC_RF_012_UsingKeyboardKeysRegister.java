d3223e62a4e732bc95718e18f3abffde420ec79c
Manu <mariavkachapally@gmail.com>
2024-11-29 18:12:15 -0500

    added more codes

--- src/test/java/registerPack/TC_RF_012_UsingKeyboardKeysRegister.java
+++ src/test/java/registerPack/TC_RF_012_UsingKeyboardKeysRegister.java
@@ -2,12 +2,16 @@ package registerPack;
 
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
+
+import utils.CommonUtilities;
 
 public class TC_RF_012_UsingKeyboardKeysRegister {
 
@@ -32,7 +36,16 @@ public class TC_RF_012_UsingKeyboardKeysRegister {
 			actions.sendKeys(Keys.TAB).perform();
 			
 		}
-		actions.sendKeys("Maria").sendKeys(Keys.TAB).sendKeys("Jo").sendKeys(Keys.TAB).sendKeys(CommonUtilities.generateBrandnewEmail()).sendKeys(Keys.TAB).sendKeys("01236547898").sendKeys(Keys.TAB).build().perform();
+		actions.sendKeys("Maria").sendKeys(Keys.TAB).sendKeys("Jo").sendKeys(Keys.TAB)
+		.sendKeys(CommonUtilities.generateBrandnewEmail()).sendKeys(Keys.TAB)
+		.sendKeys("1236547893").sendKeys(Keys.TAB).sendKeys("JoJo").sendKeys(Keys.TAB)
+		.sendKeys("JoJo").sendKeys(Keys.TAB).sendKeys(Keys.TAB).sendKeys(Keys.TAB).sendKeys(Keys.SPACE).sendKeys(Keys.TAB)
+		.sendKeys(Keys.ENTER).build().perform();
+		Thread.sleep(3000);
 		
+		//Assert.assertTrue(driver.findElement(By.xpath("//ul[@class='breadcrumb']//a[text()='Success']")).isDisplayed());
+		Assert.assertTrue(driver.findElement(By.xpath("//ul[@class='breadcrumb']//a[text()='Success']")).isDisplayed());
+		Assert.assertTrue(driver.findElement(By.xpath("//a[@class='list-group-item'][text()='Logout']")).isDisplayed());
+		driver.quit();
 	}
 }
